342c741818901a59ec456b0ea1a3e2ddf6bab4e6
Richard Buchanan <richardbuchanan@richardbuchanan.com>
2017-07-07 18:47:32 -0400

    Updated sources

---
+++ profiles/uikit_api/sources/uikit_admin-8.x-2.x/js/uikit_admin.theme.js
@@ -0,0 +1,21 @@
+/**
+ * @file
+ * Attaches behaviors for the UIkit Admin theme.
+ */
+
+(function ($, Drupal) {
+  'use strict';
+
+  Drupal.behaviors.uikitAdminDropbuttons = {
+    attach: function (context, settings) {
+      var button_group = $(context).find('.uk-button-group').once('uk-dropdown');
+      if (button_group.length) {
+        $(button_group).each(function () {
+          var dropdown = $(this).find('.uk-dropdown');
+          dropdown.css('min-width', $(this).outerWidth(false) - 6);
+        })
+      }
+    }
+  }
+
+})(jQuery, Drupal);
