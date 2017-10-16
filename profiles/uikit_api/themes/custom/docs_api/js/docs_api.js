f628c4c52715f9b9adf525d9dc6637dafd9a8c2d
Richard Buchanan <richardbuchanan@richardbuchanan.com>
2017-10-16 08:06:22 -0400

    Major updates to how the API module is themed and added required API 7.x-2.x modules

--- profiles/uikit_api/themes/custom/docs_api/js/docs_api.js
+++ profiles/uikit_api/themes/custom/docs_api/js/docs_api.js
@@ -1,21 +1,33 @@
 /**
  * @file
- * Attaches behaviors to the UIkit theme.
+ * Attaches behaviors to the Docs API theme.
  */
 
 (function ($) {
   'use strict';
 
-  Drupal.behaviors.docsAPI = {
+  Drupal.behaviors.docsAPIExternalLinks = {
     attach: function () {
       $('a').each(function () {
-        if (this.hostname !== location.host) {
-          // Make sure all external links open in a new tab.
+        if (this.hostname !== location.host && this.hostname !== 'ftp.drupal.org') {
+          // Make sure all external links open in a new tab, except project
+          // download files.
           $(this).prop('target', '_blank');
 
           if (/drupal/i.test(this.hostname)) {
             $(this).addClass('drupal-api');
           }
+        }
+      });
+    }
+  };
+
+  Drupal.behaviors.docsAPIDrupalAPILinks = {
+    attach: function () {
+      $('code').find('a').each(function () {
+        if (/drupal/i.test(this.hostname)) {
+          // Add the drupal-api class to linkable elements in API code elements.
+          $(this).addClass('drupal-api');
         }
       });
     }
