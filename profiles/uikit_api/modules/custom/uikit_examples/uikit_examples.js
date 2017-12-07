130b7f7455d25e2403fb1c2c09de3bb40908b5dd
Richard Buchanan <richardbuchanan@richardbuchanan.com>
2017-12-07 12:49:17 -0500

    Updated examples

--- profiles/uikit_api/modules/custom/uikit_examples/uikit_examples.js
+++ profiles/uikit_api/modules/custom/uikit_examples/uikit_examples.js
@@ -4,7 +4,13 @@
   var $iframe = $('#uikit-tests');
 
   $iframe.load(function () {
-    this.style.height = (this.contentWindow.document.body.offsetHeight + 200) + 'px';
+    // Resize the iframe to fit the content height.
+    this.style.height = (this.contentWindow.document.documentElement.offsetHeight) + 'px';
+
+    // We initially hide the iframe. After resizing, show the content and
+    // perform a click event so the container element is also resized. Otherwise
+    // we end up having a container with a smaller height than the iframe which
+    // displays a horizontal scroll bar.
     $(this).show().click();
   });
 })(jQuery);
